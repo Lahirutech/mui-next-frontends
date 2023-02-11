10f45bb21600c4d861172fc1692dc527cca76a4d
lahirutech <rmllbrathnayaka@gmail.com>
2023-02-11 22:02:46 +0530

    initialize

---
+++ pages/register.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../layout/layout';
+
+export default function Register() {
+  return (
+    <Layout>
+      <Head>
+        <title>Register</title>
+      </Head>
+      Login
+    </Layout>
+  );
+}
