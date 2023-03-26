f645166e78b4c7493469f41d9dd27ab771a60ce3
lahirutech <rmllbrathnayaka@gmail.com>
2023-03-27 00:16:45 +0530

    feat:adding the background image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import { Box } from '@mui/system';
 import React from 'react';
 
 export default function index() {
-  return <div>index</div>;
+  return (
+    <Box
+      margin={0}
+      padding={0}
+      boxSizing='border-box'
+      width={'100%'}
+      height='100vh'
+      sx={{
+        background:
+          'linear-gradient(rgba(0,0,0,0.3),transparent),url(assets/images/background.png)',
+      }}
+    ></Box>
+  );
 }
