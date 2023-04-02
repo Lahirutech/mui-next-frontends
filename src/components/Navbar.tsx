ac4a7ff05a3157449af3c8d9881ecb38571544a3
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-02 22:47:45 +0530

    feat:add navbar icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,24 @@
-import { AppBar, Box, Typography, InputBase } from '@mui/material';
-import DataUsageOutlinedIcon from '@mui/icons-material/DataUsageOutlined';
-import { SearchDiv, StyledToolbar } from './Toolbar/StyledToolbar';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Badge,
+  Typography,
+  InputBase,
+  MenuItem,
+  Menu,
+} from '@mui/material';
+import { DataUsageOutlined, Mail, Notifications } from '@mui/icons-material';
+import {
+  IconSet,
+  ProfileIcon,
+  SearchDiv,
+  StyledToolbar,
+} from './Toolbar/StyledToolbar';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position='sticky'>
       <StyledToolbar>
@@ -14,7 +30,7 @@ const Navbar = () => {
           >
             Analytics
           </Typography>{' '}
-          <DataUsageOutlinedIcon
+          <DataUsageOutlined
             fontSize='large'
             sx={{ display: { xs: 'block', sm: 'none' } }}
           />
@@ -22,8 +38,52 @@ const Navbar = () => {
         <SearchDiv>
           <InputBase placeholder='Search...' />
         </SearchDiv>
-        <Box>Iconset</Box>
+        <IconSet>
+          <Badge
+            badgeContent={4}
+            color='secondary'
+          >
+            <Mail />
+          </Badge>
+          <Badge
+            badgeContent={4}
+            color='secondary'
+          >
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src='https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+            onClick={(e) => setOpen(true)}
+          />
+        </IconSet>
+        <ProfileIcon>
+          {' '}
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src='https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+            onClick={(e) => setOpen(true)}
+          />
+        </ProfileIcon>
       </StyledToolbar>
+      <Menu
+        id='demo-positioned-menu'
+        aria-labelledby='demo-positioned-button'
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
--- src/components/Toolbar/StyledToolbar.tsx
+++ src/components/Toolbar/StyledToolbar.tsx
@@ -1,4 +1,4 @@
-import { Toolbar, styled } from '@mui/material';
+import { Toolbar, styled, Box } from '@mui/material';
 
 export const StyledToolbar = styled(Toolbar)({
   display: 'flex',
@@ -12,4 +12,22 @@ export const SearchDiv = styled('div')(({ theme }) => ({
   borderRadius: theme.shape.borderRadius,
   height: '40px',
   display: 'flex',
+}));
+
+export const IconSet = styled(Box)(({ theme }) => ({
+  display: 'none',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '20px',
+
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}));
+
+export const ProfileIcon = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
 }));
