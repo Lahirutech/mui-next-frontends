2ae4f4c8ed367c631914360780d1393297b35da5
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-07 09:26:11 +0530

    feat:adding singalton to db connection

--- database/config.ts
+++ database/config.ts
@@ -1,12 +1,19 @@
 import mongoose from 'mongoose';
+let isConnected: boolean;
 
 const connectMongo = async () => {
+  if (isConnected) {
+    console.log('connected using the old instance');
+    return Promise.resolve(true);
+  }
+
   try {
     const { connection } = await mongoose.connect(
       process.env.MONGO_URL as string
     );
 
     if (connection.readyState == 1) {
+      isConnected = true;
       return Promise.resolve(true);
     }
   } catch (err) {
