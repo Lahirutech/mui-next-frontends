81248030edec1943f0d239ed37f1c9ed3f29bb97
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-10 22:45:45 +0530

    initialize with sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,26 +7,5 @@ import ProTip from '../src/ProTip';
 import Copyright from '../src/Copyright';
 
 export default function Home() {
-  return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI - Next.js example in TypeScript
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+  return <Container maxWidth='lg'></Container>;
 }
