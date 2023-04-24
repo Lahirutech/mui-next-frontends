5c907659ec6f27569596788e859fb9a6ddc48050
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-24 10:51:00 +0530

    feat:add chat view

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,7 +1,38 @@
+import Image from 'next/image';
 import React from 'react';
-
+import styles from './chat.module.scss';
+import { Messages } from '../messages/Messages';
+import { Input } from '../input/Input';
 function Chat() {
-  return <div>Chat</div>;
+  return (
+    <div className={styles.chat}>
+      <div className={styles.chatInfo}>
+        <span>{'User Name'}</span>
+        <div className={styles.chatIcon}>
+          <Image
+            src='/image/cam.png'
+            alt=''
+            width={30}
+            height={30}
+          />
+          <Image
+            src='/image/add.png'
+            alt=''
+            width={30}
+            height={30}
+          />{' '}
+          <Image
+            src='/image/more.png'
+            alt=''
+            width={30}
+            height={30}
+          />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
 }
 
 export default Chat;
