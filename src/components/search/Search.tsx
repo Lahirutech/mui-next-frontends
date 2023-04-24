74924e42bf8713c2fc05e6ff25f2b6a5afed865d
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-24 10:52:08 +0530

    feat:add search view

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './search.module.scss';
 
 function Search() {
-  return <div>Search</div>;
+  const [err, setErr] = useState(null);
+  const [user, setUser] = useState(null);
+
+  return (
+    <div className={styles.search}>
+      <div className={styles.searchForm}>
+        <input
+          type='text'
+          placeholder='Find a user'
+          onKeyDown={() => ''}
+          onChange={() => ''}
+          value={''}
+        />
+      </div>
+      {err && <span>User not Found</span>}
+      {user && (
+        <div
+          className={styles.userChat}
+          onClick={() => ''}
+        >
+          <img
+            src={user}
+            alt=''
+          />
+          <div className={styles.userChatInfo}>
+            <span>{user}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Search;
