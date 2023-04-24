095b9e51f0c27d9f0dd6fdbb2e4412d6e33a5b08
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-24 10:51:58 +0530

    feat:add nav

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import styles from './navbar.module.scss';
+import Image from 'next/image';
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <span className='logo'>Lama chat</span>
-      <div className='user'>
-        <img
+      <span className={styles.logo}>Lama chat</span>
+      <div className={styles.user}>
+        <Image
           src={''}
           alt=''
+          height={24}
+          width={24}
+          style={{
+            backgroundColor: '#ddddf7',
+            borderRadius: '50%',
+            objectFit: 'cover',
+          }}
         />
         <span>{'Dispaly Name'}</span>
         <button>logout</button>
