30a185e86762a05b6cbc037b6b625b00ceec843a
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-25 16:21:05 +0530

    feat:new theme changes

--- src/theme.ts
+++ src/theme.ts
@@ -1,30 +1,13 @@
-import { Roboto } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { createTheme } from '@mui/material';
 
-export const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  fallback: ['Helvetica', 'Arial', 'sans-serif'],
-});
-
-// Create a theme instance.
-const theme = createTheme({
+export const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#1760a5',
+      light: 'skyblue',
     },
     secondary: {
-      main: '#19857b',
+      main: '#15c630',
     },
-    error: {
-      main: red.A400,
-    },
-  },
-  typography: {
-    fontFamily: roboto.style.fontFamily,
   },
 });
-
-export default theme;
