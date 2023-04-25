723d89fa24b63b9523da43a2751009bcb36d4af0
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-25 16:20:38 +0530

    feat:pass toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,26 @@
 import Box from '@mui/material/Box';
-import { Stack } from '@mui/material';
+import { PaletteMode, Stack, createTheme } from '@mui/material';
 import { LeftBar } from '../src/components/LeftBar';
 import Content from '../src/components/Content';
 import RightBar from '../src/components/RightBar';
 import Navbar from '../src/components/Navbar';
+import { useState } from 'react';
 
-export default function Home() {
+export default function Home(props: {
+  toggleTheme: React.MouseEventHandler<HTMLAnchorElement>;
+}) {
   return (
-    <Box>
+    <Box
+      bgcolor={'background.default'}
+      color={'text.primary'}
+    >
       <Navbar />
       <Stack
         direction='row'
         spacing={2}
-        alignItems={'center'}
         justifyContent={'space-between'}
       >
-        <LeftBar />
+        <LeftBar toggleTheme={props.toggleTheme} />
         <Content />
         <RightBar />
       </Stack>
