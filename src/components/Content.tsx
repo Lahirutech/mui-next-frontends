7581e61550d553445d710e2b06ee60066dfdd512
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-25 15:04:46 +0530

    feat: add multiple Offers

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,11 +1,16 @@
 import Box from '@mui/material/Box';
+import Offer from './Offer';
+import AddOffer from './AddOffer';
 const Content = () => {
   return (
-    <Box
-      flex={4}
-      sx={{ backgroundColor: 'green' }}
-    >
-      Content
+    <Box flex={4}>
+      <Offer />
+      <Offer />
+      <Offer />
+      <Offer />
+      <Offer />
+      <Offer />
+      <AddOffer />
     </Box>
   );
 };
