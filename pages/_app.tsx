9ba1c2bf08ccba369e6e121bed73a62dc3179c41
lahirutech <rmllbrathnayaka@gmail.com>
2023-04-25 16:15:57 +0530

    feat:add toggle theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../src/theme';
+import { theme } from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import { PaletteMode } from '@mui/material';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -14,17 +15,45 @@ export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const [selectedTheme, setSelectedTheme] =
+    React.useState<PaletteMode>('light');
+  const [avtiveTheme, setAvtiveTheme] = React.useState(lightTheme);
+
+  const toggleTheme = () => {
+    const desiredTheme = selectedTheme === 'light' ? 'dark' : 'light';
+    setSelectedTheme(desiredTheme);
+    setAvtiveTheme(desiredTheme === 'light' ? lightTheme : darkTheme);
+  };
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta
+          name='viewport'
+          content='initial-scale=1, width=device-width'
+        />
       </Head>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={avtiveTheme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <Component
+          {...pageProps}
+          toggleTheme={toggleTheme}
+        />
       </ThemeProvider>
     </CacheProvider>
   );
